c42e1c3ce822a190db8413dc9a19841410779164
TaynaraW <161765041+taynaraww@users.noreply.github.com>
2024-03-14 20:16:56 -0300

    Add files via upload

---
+++ Csharp/DIAGONAL_NEGATIVOS.cs
@@ -0,0 +1,37 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+       
+        int n, i, j, cont;
+        
+        Console.Write("Qual a ordem da matriz? ");
+        n = int.Parse(Console.ReadLine());
+
+        int[,] mat = new int[n, n];
+
+        for(i=0; i<n; i++) {
+            for(j=0; j<n; j++) {
+                Console.Write("Elemento [" + i + "," + j + "]: ");
+                mat[i,j] = int.Parse(Console.ReadLine());
+            }  
+        }
+        Console.WriteLine("DIAGONAL PRINCIPAL: ");
+        for (i = 0; i < n; i++) {
+            Console.Write(mat[i,i] + " ");
+        }
+        Console.WriteLine();
+        cont = 0;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (mat[i,j] < 0) {
+                    cont++;
+                }
+            }
+
+        }
+           
+     Console.WriteLine("QUANTIDADE NEGATIVOS = " + cont);
+
+            
+    }
+
+}
